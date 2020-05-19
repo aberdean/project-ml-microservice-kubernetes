9f98adea827d2568931c746dbbc84e541ae85537
Antonella Bernobich Dean <aberdean@users.noreply.github.com>
2020-05-19 12:37:26 +0200

    Add script to run Kubernetes

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,21 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+ dockerpath="aberdean/predictionapp"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run predictionapp\
+    --generator=run-pod/v1\
+    --image=$dockerpath\
+    --port=80 --labels app=predictionapp
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward predictionapp 8000:80
 
