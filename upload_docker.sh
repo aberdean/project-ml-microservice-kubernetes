0c93f0c5a14c98ff0499227b8c522c7231d57ec5
Antonella Bernobich Dean <aberdean@users.noreply.github.com>
2020-05-19 12:08:45 +0200

    Push container to Docker Hub

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,15 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=aberdean/predictionapp
 
 # Step 2:  
 # Authenticate & tag
+docker login --username aberdean
+docker tag predictionapp $dockerpath
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
+
